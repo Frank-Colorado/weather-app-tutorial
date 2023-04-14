e238e88fc1cbc9d7c9279a0225c9c18f2ae47465
Frank-Colorado <chawnkiee@gmail.com>
2023-04-14 19:48:16 -0400

    added functions 'searchCity' and 'getWeather'

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
 // API test 
+// Need: data.temp, data.temp_min, data.temp_max, data.weather[0].main
+
+const searchInput = document.getElementById('city-input')
+
 
 const options = {
 	method: 'GET',
@@ -8,8 +12,24 @@ const options = {
 	}
 };
 
-fetch('https://open-weather13.p.rapidapi.com/city/tampa', options)
+// This is a function called getWeather.
+// It has 1 parameter called 'city' 
+const getWeather = (city) => {
+fetch(`https://open-weather13.p.rapidapi.com/city/${city}`, options)
 	.then(response => response.json())
 	.then(data => console.log(data))
 	.catch(err => console.error(err));
+}
 
+
+
+
+// This is a function called searchCity. 
+// It has 0 parameter
+// It will be called when the 'Search' btn is clicked
+// It will define a variable called 'search' which uses 'searchInput.value' as its value
+// It will then call the 'getWeather' function and use 'search' as a parameter
+const searchCity = () => {
+        let search = searchInput.value 
+        getWeather(search.toLowerCase())
+    }
